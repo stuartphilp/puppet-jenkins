9a0116698d4911972e5c894efd8e7b52d95c1e90
Stuart Philp <stuartphilp@gmail.com>
2015-10-02 15:23:09 -0400

    disable cert check first

--- manifests/cli.pp
+++ manifests/cli.pp
@@ -29,7 +29,7 @@ class jenkins::cli {
   $port = jenkins_port()
 
   # The jenkins cli command with required parameter(s)
-  $cmd = "java -jar ${jar} -s https://localhost:${port} -noCertificateCheck"
+  $cmd = "java -jar ${jar} -noCertificateCheck -s https://localhost:${port}"
 
   # Do a safe restart of Jenkins (only when notified)
   exec { 'safe-restart-jenkins':
